c263bbb499f11e7216a50ec1b4b63793f42fd350
Knoperaune <hubert.rall@students.fhv.at>
2012-10-22 12:40:12 +0200

    initialize

---
+++ PipesAndFilters/src/pipesandfilters/PipesAndFilters.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipesandfilters;
+
+/**
+ *
+ * @author Kno
+ */
+public class PipesAndFilters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
